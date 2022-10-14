d6105adb4487b62fcaaf42e8a4928e2aeee0e357
yazanismail1 <yalfarra@outlook.com>
2022-10-14 19:24:20 +0000

    update

--- oop_project/main.py
+++ oop_project/main.py
@@ -1,17 +1,18 @@
-import requests
+# import requests
 
-get_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1'
-post_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1'
-put_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1/[Object ID]'
-delete_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1/3'
+# get_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1'
+# post_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1'
+# put_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1/[Object ID]'
+# delete_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1/3'
 
-data = {"sheet1":{'socialMedia': 'Snapchat', 'userName': 'ggg', 'password': "123wfwfw245"}}
+# data = {"sheet1":{'socialMedia': 'Snapchat', 'userName': 'ggg', 'password': "123wfwfw245"}}
 
-d = requests.delete(delete_url)
-# p = requests.put(put_url, json)
-# p = requests.post(post_url, json=data)
-# g = requests.get(get_url).json()
-# for i in range(len(g["sheet1"])):
-#     if g["sheet1"][i]["socialMedia"] == "Insta":
-#         print(g["sheet1"][i])
-print(d)
+# # d = requests.delete(delete_url)
+# # p = requests.put(put_url, json)
+# # p = requests.post(post_url, json=data)
+# r = requests.get(get_url).json()
+
+# for i in range(len(r["sheet1"])):
+#     if r["sheet1"][i]["socialMedia"] == "Facebook":
+#         x =  r["sheet1"][i]["socialMedia"] + " --> " + "User Name: " + r["sheet1"][i]["userName"] + ", " + "Password: " + str(r["sheet1"][i]["password"])
+#         print(x)
