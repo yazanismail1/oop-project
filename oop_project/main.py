5da7133879a2096b9eb9f528e0740f9a88fb9cea
Razan-abdullah <razanaalabdallah017@gmail.com>
2022-10-15 12:26:02 +0000

    generate_pass

--- oop_project/main.py
+++ oop_project/main.py
@@ -1,6 +1,4 @@
-# import requests
-import random
-import array
+
 
 # get_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1'
 # post_url = 'https://api.sheety.co/b7d8ad9f87b711f1445e9821751afa0e/oopProject/sheet1'
@@ -19,42 +17,3 @@ import array
 #         x =  r["sheet1"][i]["socialMedia"] + " --> " + "User Name: " + r["sheet1"][i]["userName"] + ", " + "Password: " + str(r["sheet1"][i]["password"])
 #         print(x)
 
-
-def Generat_Password():
-
-    PassWord_Len = 8
-
-    DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-    LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-    UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                         'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-    SYMBOLS = ['@', '#', '$', '%', '=', ':', '?',
-               '.', '/', '|', '~', '>', '*', '(', ')', '<']
-
-    COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
-
-    digit = random.choice(DIGITS)
-    upper = random.choice(UPCASE_CHARACTERS)
-    lower = random.choice(LOCASE_CHARACTERS)
-    symbol = random.choice(SYMBOLS)
-
-    Pass = digit + upper + lower + symbol
-
-    for x in range(PassWord_Len - 4):
-        Pass = Pass + random.choice(COMBINED_LIST)
-
-        pass_list = array.array('u', Pass)
-        random.shuffle(pass_list)
-
-    password = ""
-    for x in pass_list:
-        password = password + x
-
-    return password
-
-
-print(Generat_Password())
